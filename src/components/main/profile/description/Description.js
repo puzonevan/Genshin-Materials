993431e67ea95c4979a8ab350532e06677c824c0
‘Evan <puzonevan@gmail.com>
2022-03-09 21:25:09 -0800

    Description and Profile positioning css

--- src/components/main/profile/description/Description.js
+++ src/components/main/profile/description/Description.js
@@ -4,8 +4,10 @@ import './Description.css';
 export function Description(props){
     return (
         <section className="description" id="description">
-            <h2>Character Name</h2>
-            <p>Character Description</p>
+            <h2>Zhongli</h2>
+            <h3>Sub title</h3>
+            <p>Wangsheng Funeral Parlor's mysterious consultant. Handsome, elegant, and surpassingly learned. Though no one knows where Zhongli is from, he is a master of courtesy and rules. From his seat at Wangsheng Funeral Parlor, he performs all manner of rituals.</p>
+            <hr />
             <div>
                 {/* Icons */}
                 <div>
@@ -18,6 +20,7 @@ export function Description(props){
                     Nation
                 </div>
             </div>
+            <hr />
         </section>
     );
 };
