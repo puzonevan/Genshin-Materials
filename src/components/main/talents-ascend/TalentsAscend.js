6223a9a15c0c5b9154382f0e64afdbf62318576a
‘Evan <puzonevan@gmail.com>
2022-03-18 20:26:04 -0700

    menu state setup

--- src/components/main/talents-ascend/TalentsAscend.js
+++ src/components/main/talents-ascend/TalentsAscend.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './TalentsAscend.css';
 import { Display } from './display/Display';
 import { Menu } from './menu/Menu';
@@ -7,13 +8,18 @@ import { Menu } from './menu/Menu';
 export function TalentsAscend(props){
 
     const { talents, ascension } = props;
+    const [talentLevel, setTalentLevel] = useState(2);
+    const [ascendLevel, setAscendLevel] = useState(1);
+
+    const changeTalentLevel = (level) => setTalentLevel(level);
+    const changeAscendLevel = (level) => setAscendLevel(level);
 
     return (
         <section className="talents-ascend" id="talents-ascend">
             <Display name={"Talents"} materials={talents}/>
-            <Menu />
+            <Menu name={"talent"} low={2} high={10}/>
             <Display name={"Ascension"} materials={ascension}/>
-            <Menu />
+            <Menu name={"ascend"} low={1} high={6}/>
             <img src="" alt="img1" />
             <img src="" alt="img2" />
         </section>
--- src/components/main/talents-ascend/menu/Menu.js
+++ src/components/main/talents-ascend/menu/Menu.js
@@ -3,14 +3,17 @@ import './Menu.css';
 
 export function Menu(props){
     
+    const { name, low, high } = props;
+    const options = [];
+    for(let i = low; i <= high; i++){
+        options.push(<td key={`${name}-${i}`}>{i}</td>);
+    }
+
     return (
         <table>
             <tbody>
                 <tr>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>3</td>
-                    <td>4</td>
+                    {options}
                 </tr>
             </tbody>
         </table>
