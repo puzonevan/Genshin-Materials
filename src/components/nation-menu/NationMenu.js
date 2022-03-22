27b114ab7f7937e90c7331c9e5c847c3b961d486
‘Evan <puzonevan@gmail.com>
2022-03-22 12:08:51 -0700

    set up nation menu images

--- src/components/nation-menu/NationMenu.js
+++ src/components/nation-menu/NationMenu.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './NationMenu.css';
+import mondstadt from './mondstadt-symbol.png';
+import liyue from './liyue-symbol.png';
+import inazuma from './inazuma-symbol.png';
+
+
 
 export function NationMenu(props){
     
@@ -15,15 +20,15 @@ export function NationMenu(props){
     return (
         <nav className="nation-menu" id="nation-menu">
             <figure>
-                <img onClick={(e) => handleChangeNation(e, 'Mondstadt')} alt="Mondstadt logo"/>
+                <img onClick={(e) => handleChangeNation(e, 'Mondstadt')} src={mondstadt} alt="Mondstadt logo"/>
                 <figcaption onClick={(e) => handleChangeNation(e, 'Mondstadt')}>Mondstadt</figcaption>
             </figure>
             <figure>
-                <img onClick={(e) => handleChangeNation(e, 'Liyue')} alt="Liyue logo"/>
+                <img onClick={(e) => handleChangeNation(e, 'Liyue')} src={liyue} alt="Liyue logo"/>
                 <figcaption onClick={(e) => handleChangeNation(e, 'Liyue')}>Liyue</figcaption>
             </figure>
             <figure>
-                <img onClick={(e) => handleChangeNation(e, 'Inazuma')} alt="Inazuma logo"/>
+                <img onClick={(e) => handleChangeNation(e, 'Inazuma')} src={inazuma} alt="Inazuma logo"/>
                 <figcaption onClick={(e) => handleChangeNation(e, 'Inazuma')}>Inazuma</figcaption>
             </figure>
         </nav>
