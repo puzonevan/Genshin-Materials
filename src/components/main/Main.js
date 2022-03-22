56d6b595992d571464ada5d0f1d6eb59bceb9b7b
‘Evan <puzonevan@gmail.com>
2022-03-22 11:57:17 -0700

    remove weapons and artifacts

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Main.css';
 import { Profile } from './profile/Profile';
 import { TalentsAscend } from './talents-ascend/TalentsAscend';
-import { WeapArt } from './weapons-artifacts/WeapArt';
 import { AbilitiesConst } from './abilities-const/AbilitiesConst';
 import { getTalentCosts, getAscendCosts } from '../../util/character';
 
@@ -22,7 +21,6 @@ export function Main(props) {
                 talents={talents}
                 ascension={ascension}
             />
-            <WeapArt />
             <AbilitiesConst 
                 currentCharacter={currentCharacter}
             />
