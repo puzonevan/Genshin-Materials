fe7cd28f60f182cce51bd2f3f11cc122bc6c4540
‘Evan <puzonevan@gmail.com>
2022-03-22 22:15:46 -0700

    change region on profile based on character

--- src/components/main/profile/description/Description.js
+++ src/components/main/profile/description/Description.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './Description.css';
+import mondstadt from './mondstadt-symbol.png';
+import liyue from './liyue-symbol.png';
+import inazume from './inazuma-symbol.png';
 
 export function Description(props){
 
     const { element, characterInfo } = props;
     const weapon = characterInfo["weapontype"];
-    const region = characterInfo["region"]
+    const region = characterInfo["region"];
+
+    const nationImage = (name) => {
+        console.log(name);
+        switch(name){
+            case 'mondstadt': 
+                return mondstadt;
+            case 'liyue': 
+                return liyue;
+            case 'inazuma': 
+                return inazume;
+            default: 
+                return mondstadt;
+        }
+    }
 
     return (
         <section className="description" id="description">
@@ -19,9 +36,7 @@ export function Description(props){
                 <div>
                     {weapon}
                 </div>
-                <div>
-                    {region}
-                </div>
+                <img src={nationImage(region.toLowerCase())} alt="character nation"/>
             </div>
             <hr />
         </section>
