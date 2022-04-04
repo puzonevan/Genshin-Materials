99139d04a4b76a7bb3d25fa3440d4da1c947ecd5
‘Evan <puzonevan@gmail.com>
2022-04-03 19:27:57 -0700

    clarifying comments Menu.js

--- src/components/main/talents-ascend/menu/Menu.js
+++ src/components/main/talents-ascend/menu/Menu.js
@@ -3,7 +3,14 @@ import './Menu.css';
 
 export function Menu(props){
     
+    // Props 
+    //  - name: either talent or ascend
+    //  - low: lowest value of menu 
+    //  - high: highest value of menu 
+    //  - changeLevel: change the current level state 
     const { name, low, high, changeLevel } = props;
+
+    // td list of options for creating menu
     const options = [];
     for(let i = low; i <= high; i++){
         options.push(<td onClick={() => changeLevel(i)} key={`${name}-${i}`}>{i}</td>);
