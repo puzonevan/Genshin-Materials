712b1c128968fe5419faa37c4897e6cdbdff2349
‘Evan <puzonevan@gmail.com>
2022-04-03 19:21:03 -0700

    clarifying comments Display.js

--- src/components/main/talents-ascend/display/Display.js
+++ src/components/main/talents-ascend/display/Display.js
@@ -4,8 +4,17 @@ import { Material } from '../material/Material';
 
 export function Display(props) {
 
+    // Props 
+    //  - name: character name
+    //  - materials: materials of character
     const { name, materials } = props;
 
+    /**
+     * createMaterial create li element of materials name and count
+     * @param {*} name name of material
+     * @param {*} count number of material
+     * @returns li element
+     */
     const createMaterial = (name, count) => {
         return (
             <li key={`${name}-${count}`}>
@@ -14,6 +23,7 @@ export function Display(props) {
         );
     };
     
+    // maps materials to a list of li elements 
     const materialsList = Object.keys(materials).map(name => createMaterial(name, materials[name]));
 
     return (
